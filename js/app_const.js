30a91c80347409d1c4c1ab50e00b7bed06c54de3
DMTGIT <dmtyagigithub1997@gmail.com>
2023-12-01 10:58:27 +0530

    commit(01-12-2023)

---
+++ js/app_const.js
@@ -0,0 +1,384 @@
+var menCategory = {
+    topWear: [
+        "T-Shirts",
+        "casual Shirts",
+        "Formal Shirts",
+        "Sweatshirts",
+        "Sweaters",
+        "Jackets",
+        "Blazers & Coats",
+        "Suits",
+        "Rain Jackets"
+    ],
+    bottomWear: [
+        "Jeans",
+        "casual Trousers",
+        "Formal Trousers",
+        "Shorts",
+        "Track Pants & Joggers",
+    ],
+    indianAndFestiveWear: [
+        'Kurtas & Kurts Sets',
+        'Sherwanis',
+        'Nehru Jackets',
+        'Dhotis',
+    ],
+    innerWearAndSleepwear: [
+        "Briefs & Trunks",
+        "Boxers",
+        "Vests",
+        "Sleepwear & Loungewear",
+        "Thermal Wear"
+    ],
+    sportsAndActiveWear: [
+        'Sports Shoes',
+        'Sports Sandals',
+        'Active T-Shirts',
+        'Track Pants & Shorts',
+        'Jackets & Sweatshirts',
+        'Sports Accessories',
+        'Swimwear'
+    ],
+    fashionAccessories: [
+        'Wallet',
+        'Perfumes & Body Mist',
+        'Belt',
+        'Helmet',
+        'Trimmers',
+        'Deodorant',
+        'Ties, Cufflinks & Pouches',
+        'Caps & Hats',
+        'Phone Cases',
+        'Rings & Wristwear',
+        'Accessory Gift Set'
+    ],
+    footwear: [
+        'Casual Shoes',
+        'Formal Shoes',
+        'Sneakers',
+        'Sports Shoes',
+        'Sandals & Floaters',
+        'Flip Flops',
+        'Socks',
+    ],
+    gadgets: [
+        'Smart Watches',
+        'Fitness Bands',
+        'Headphones',
+        'Speakers',
+    ]
+};
+var womenCategory = {
+    indianAndFusionwear: [
+        'Sarees',
+        'Kurtas & Suits',
+        'Dupattas',
+        'Lehenga Choli',
+        'Skirts & Palazzos',
+        'Anarkali Suits',
+        'Jackets',
+        'Shawls',
+        'Dress Material',
+        'Lingerie Sets'
+    ],
+    westernWear: [
+        "Jeans",
+        'Tops',
+        'Dresses',
+        'Tshirts',
+        'Trousers & Capris',
+        'Co-ords',
+        'Playsuits',
+        'Jumpsuits',
+        'Sweaters & Cardigans',
+        'Sweatshirts & Hoodies',
+        'Shorts & Skirts',
+        'Jacket & Waist',
+        'Blazers & Waistcoats'
+    ],
+    indianAndFestiveWear: [
+        'Kurtas & Kurts Sets',
+        'Sherwanis',
+        'Nehru Jackets',
+        'Dhotis',
+    ],
+    lingerieAndSleepwear: [
+        'Bra',
+        'Briefs',
+        'ShapeWear',
+        'Sleepwear',
+        'Camisoles & Thermals',
+        'Swimwear'
+    ],
+    sportsAndActiveWear: [
+        'Clothing',
+        'footwear',
+        'Sports Accessories',
+        'Sports Equipment'
+    ],
+    BeautyAndPersonalCare: [
+        'Makeup',
+        'Skincare',
+        'Premium Beauty',
+        'Lipsticks',
+        'Fragrances'
+    ],
+    footwear: [
+        'Flats',
+        'Casual Shoes',
+        'Heels',
+        'Boots',
+        'Sports Shoes & Floaters'
+    ],
+    gadgets: [
+        'Smart Wearable',
+        'Fitness Gadgets',
+        'Headphones',
+        'Speakers',
+    ],
+    jewellery: [
+        'Earrings',
+        'Feshion Jewellery',
+        'Fine Jewellery'
+    ]
+};
+var kidsCategory = {
+    boysClothing: [
+        'T-Shirts',
+        'Clothings',
+        'shoes',
+        'Jeans',
+        'Jackets, Sweater & Sweatshirts',
+        'Innerwear, Thermals',
+        'Nightwear & Loungewear',
+        'value Packs',
+        'Party Wear',
+        'jackts, Sweater & sweatshirts',
+        'Track Pants & Pyjamas',
+        'Ethnic Wear',
+        'Trousers',
+        'Shirts'
+    ],
+    girlsClothing: [
+        'Dresses',
+        'Tops',
+        'Tshirts',
+        'Clothings',
+        'Lehenga choli',
+        'Kurta Sets',
+        'Party wear',
+        'Dungarees & Jumpsuits',
+        'Skirts & shorts',
+        'Tights & Leggings',
+        'Jeans, Trousers & Capris',
+        'Jackets, Sweater & Sweatshirts',
+        'Innerwear, Thermals',
+        'Nightwear & Loungewear',
+        'value Packs'
+    ],
+    toysAndGames: [
+        'Learning & Development',
+        'Activity Toys',
+        'Soft Toys',
+        'Action Figure / Play set'
+    ],
+    infants: [
+        'Bodysuits',
+        'Rompers & Sleepshirts',
+        'Clothing Sets',
+        'Tshirts & Tops',
+        'Dresses',
+        'Bottom wear',
+        'Winter Wear',
+        'Innerwear & Sleepwear',
+        'Infant Care'
+    ],
+    kidsAccessories: [
+        'Bags & Backpacks',
+        'Watches',
+        'Jewellery & Hair accessory',
+        'Sunglasses',
+        'Masks & Protective Gear',
+        'Caps & Hats'
+    ],
+    footwear: [
+        'Casual Shoes',
+        'Flipflops',
+        'Sports Shoes',
+        'Sandals',
+        'Flip Flops',
+        'Socks',
+        'Heels',
+        'School Shoes'
+    ],
+    brands: [
+        'H&M',
+        'Max Kids',
+        'Pantaloons',
+        'United Colors Of Benetton Kids',
+        'U.S.Polo Assn. Kids',
+        'Mothercare',
+        'HRX'
+    ]
+};
+const homeLiving = {
+    bedLinenAndFurnishing: [
+        'Bed Runners',
+        'Mattress Protectors',
+        'Bedsheets',
+        'Beddings Sets',
+        'Blankets, Quilts & Dohars',
+        'Pillows & Pillows Covers',
+        'Duvet Covers',
+        'Cushions & Pillow Covers',
+        'Bed Covers',
+        'Bedding Sets',
+    ],
+    flooring: [
+        'Floor Runners',
+        "Carpets",
+        'Floor Mats & Dhurries',
+        'Door Mats'
+    ],
+    bath: [
+        'Bath Towels',
+        'Hand & Face Towels',
+        'Beach Towels',
+        'Bathrobes',
+        'Towels Sets',
+        'Bath Rugs',
+        'Bathroom Accessories',
+        'Shower Curtains'
+    ],
+    lampsAndLighting: [
+        'Lamps',
+        'Lanterns',
+        'Candles',
+        'Table Lamps',
+        'Floor Lamps',
+        'Ceiling Lamps'
+    ],
+    homeDecor: [
+        'Plants & Planters',
+        'Aromas & Candles',
+        'Clocks',
+        'Mirrors',
+        'Wall Decor',
+        'Festive Decor',
+        'Pooja Essentials',
+        'Wall Shelves',
+        'Fountains',
+        'Showpieces & Vases',
+        'Ottoman'
+    ],
+    kitchenAndTable: [
+        'Table Runners',
+        'Dinnerware & Serveware',
+        'Cups and Mugs',
+        'Bakeware & Cookware',
+        'Kitchen Storage & Tools',
+        'Bar & Drinkware',
+        'Table Covers & Furnushings'
+    ],
+    storage: [
+        'Bins',
+        'Hangers',
+        'Organisers',
+        'Hooks & Holders',
+        'Laundary Bags'
+    ],
+    Brands: [
+        'H&M',
+        "Marks & Spencer",
+        'Home Centre',
+        'Spaces',
+        'D Decors',
+        'Story@Home',
+        'Pure Home & Living',
+        'Swayam',
+        'Raymond Home',
+        'Maspar',
+        'My Trident',
+        'Contina',
+        'Random',
+        'Ellementry',
+        'ROMEE',
+        'SEJ by Nisha Gupta'
+    ]
+};
+const beauty = {
+    makeup: [
+        'Lipstick',
+        'Lip Gloss',
+        'Lip Liner',
+        'Mascara',
+        'Eyeliner',
+        'Eye Shadow',
+        'Foundation',
+        'Blush',
+        'Kajal',
+        'Foundation',
+        'Nail Polish',
+        'Primer'
+    ],
+    skincareBathAndBody: [
+        'Body Wash',
+        'Body Lotion',
+        'Body Scrub',
+        'Body Mist',
+        'Body Oil',
+        'Hand & Foot Care',
+        'Hair Serum',
+        'Hair Oil',
+        'Hair Serum',
+        'Hair Mask',
+        'Lip Balm',
+        'Hand Cream'
+    ],
+    hairCare: [
+        'Shampoo',
+        'Conditioner',
+        'Hair Cream',
+        'Hair Oil',
+        'Hair Gel',
+        'Hair Color',
+        'Hair Serum',
+        'Hair Accessory'
+    ],
+    fragrances: [
+        'Perfumes',
+        'Deodrant',
+        'Body Mist'
+    ],
+    appliances: [
+        'Hair Straightner',
+        'Hair Dryer',
+        'Epilator'
+    ],
+    mensGrooming: [
+        'Trimmers',
+        'Bread Oil',
+        'Hair Wax'
+    ],
+    beautyGiftAndMakeupSet: [
+        'Beauty Gift',
+        'Makeup Kit'
+    ],
+    topBrands: [
+        'Lakme',
+        'maybelline',
+        'LOreal',
+        'Phlips',
+        'Bath & Body Works',
+        'THE BODY SHOP',
+        'Biotique',
+        'Mamaearth',
+        'MCaffeine',
+        'Nivea',
+        'Lotus Herbals',
+        'LOreal AYURVEDA',
+        'KAMA AYURVEDA',
+        'M.A.C',
+        'Forest Essentials'
+    ]
+}
--- js/home_action.js
+++ js/home_action.js
@@ -2,392 +2,12 @@
  * 
  */
 "use strict";
-var menCategory = {
-    topWear: [
-        "T-Shirts",
-        "casual Shirts",
-        "Formal Shirts",
-        "Sweatshirts",
-        "Sweaters",
-        "Jackets",
-        "Blazers & Coats",
-        "Suits",
-        "Rain Jackets"
-    ],
-    bottomWear: [
-        "Jeans",
-        "casual Trousers",
-        "Formal Trousers",
-        "Shorts",
-        "Track Pants & Joggers",
-    ],
-    indianAndFestiveWear: [
-        'Kurtas & Kurts Sets',
-        'Sherwanis',
-        'Nehru Jackets',
-        'Dhotis',
-    ],
-    innerWearAndSleepwear: [
-        "Briefs & Trunks",
-        "Boxers",
-        "Vests",
-        "Sleepwear & Loungewear",
-        "Thermal Wear"
-    ],
-    sportsAndActiveWear: [
-        'Sports Shoes',
-        'Sports Sandals',
-        'Active T-Shirts',
-        'Track Pants & Shorts',
-        'Jackets & Sweatshirts',
-        'Sports Accessories',
-        'Swimwear'
-    ],
-    fashionAccessories: [
-        'Wallet',
-        'Perfumes & Body Mist',
-        'Belt',
-        'Helmet',
-        'Trimmers',
-        'Deodorant',
-        'Ties, Cufflinks & Pouches',
-        'Caps & Hats',
-        'Phone Cases',
-        'Rings & Wristwear',
-        'Accessory Gift Set'
-    ],
-    footwear: [
-        'Casual Shoes',
-        'Formal Shoes',
-        'Sneakers',
-        'Sports Shoes',
-        'Sandals & Floaters',
-        'Flip Flops',
-        'Socks',
-    ],
-    gadgets: [
-        'Smart Watches',
-        'Fitness Bands',
-        'Headphones',
-        'Speakers',
-    ]
-};
-var womenCategory = {
-    indianAndFusionwear: [
-        'Sarees',
-        'Kurtas & Suits',
-        'Dupattas',
-        'Lehenga Choli',
-        'Skirts & Palazzos',
-        'Anarkali Suits',
-        'Jackets',
-        'Shawls',
-        'Dress Material',
-        'Lingerie Sets'
-    ],
-    westernWear: [
-        "Jeans",
-        'Tops',
-        'Dresses',
-        'Tshirts',
-        'Trousers & Capris',
-        'Co-ords',
-        'Playsuits',
-        'Jumpsuits',
-        'Sweaters & Cardigans',
-        'Sweatshirts & Hoodies',
-        'Shorts & Skirts',
-        'Jacket & Waist',
-        'Blazers & Waistcoats'
-    ],
-    indianAndFestiveWear: [
-        'Kurtas & Kurts Sets',
-        'Sherwanis',
-        'Nehru Jackets',
-        'Dhotis',
-    ],
-    lingerieAndSleepwear: [
-        'Bra',
-        'Briefs',
-        'ShapeWear',
-        'Sleepwear',
-        'Camisoles & Thermals',
-        'Swimwear'
-    ],
-    sportsAndActiveWear: [
-        'Clothing',
-        'footwear',
-        'Sports Accessories',
-        'Sports Equipment'
-    ],
-    BeautyAndPersonalCare: [
-        'Makeup',
-        'Skincare',
-        'Premium Beauty',
-        'Lipsticks',
-        'Fragrances'
-    ],
-    footwear: [
-        'Flats',
-        'Casual Shoes',
-        'Heels',
-        'Boots',
-        'Sports Shoes & Floaters'
-    ],
-    gadgets: [
-        'Smart Wearable',
-        'Fitness Gadgets',
-        'Headphones',
-        'Speakers',
-    ],
-    jewellery: [
-        'Earrings',
-        'Feshion Jewellery',
-        'Fine Jewellery'
-    ]
-};
-var kidsCategory = {
-    boysClothing: [
-        'T-Shirts',
-        'Clothings',
-        'shoes',
-        'Jeans',
-        'Jackets, Sweater & Sweatshirts',
-        'Innerwear, Thermals',
-        'Nightwear & Loungewear',
-        'value Packs',
-        'Party Wear',
-        'jackts, Sweater & sweatshirts',
-        'Track Pants & Pyjamas',
-        'Ethnic Wear',
-        'Trousers',
-        'Shirts'
-    ],
-    girlsClothing: [
-        'Dresses',
-        'Tops',
-        'Tshirts',
-        'Clothings',
-        'Lehenga choli',
-        'Kurta Sets',
-        'Party wear',
-        'Dungarees & Jumpsuits',
-        'Skirts & shorts',
-        'Tights & Leggings',
-        'Jeans, Trousers & Capris',
-        'Jackets, Sweater & Sweatshirts',
-        'Innerwear, Thermals',
-        'Nightwear & Loungewear',
-        'value Packs'
-    ],
-    toysAndGames: [
-        'Learning & Development',
-        'Activity Toys',
-        'Soft Toys',
-        'Action Figure / Play set'
-    ],
-    infants: [
-        'Bodysuits',
-        'Rompers & Sleepshirts',
-        'Clothing Sets',
-        'Tshirts & Tops',
-        'Dresses',
-        'Bottom wear',
-        'Winter Wear',
-        'Innerwear & Sleepwear',
-        'Infant Care'
-    ],
-    kidsAccessories: [
-        'Bags & Backpacks',
-        'Watches',
-        'Jewellery & Hair accessory',
-        'Sunglasses',
-        'Masks & Protective Gear',
-        'Caps & Hats'
-    ],
-    footwear: [
-        'Casual Shoes',
-        'Flipflops',
-        'Sports Shoes',
-        'Sandals',
-        'Flip Flops',
-        'Socks',
-        'Heels',
-        'School Shoes'
-    ],
-    brands: [
-        'H&M',
-        'Max Kids',
-        'Pantaloons',
-        'United Colors Of Benetton Kids',
-        'U.S.Polo Assn. Kids',
-        'Mothercare',
-        'HRX'
-    ]
-};
-const homeLiving = {
-    bedLinenAndFurnishing: [
-        'Bed Runners',
-        'Mattress Protectors',
-        'Bedsheets',
-        'Beddings Sets',
-        'Blankets, Quilts & Dohars',
-        'Pillows & Pillows Covers',
-        'Duvet Covers',
-        'Cushions & Pillow Covers',
-        'Bed Covers',
-        'Bedding Sets',
-    ],
-    flooring: [
-        'Floor Runners',
-        "Carpets",
-        'Floor Mats & Dhurries',
-        'Door Mats'
-    ],
-    bath: [
-        'Bath Towels',
-        'Hand & Face Towels',
-        'Beach Towels',
-        'Bathrobes',
-        'Towels Sets',
-        'Bath Rugs',
-        'Bathroom Accessories',
-        'Shower Curtains'
-    ],
-    lampsAndLighting: [
-        'Lamps',
-        'Lanterns',
-        'Candles',
-        'Table Lamps',
-        'Floor Lamps',
-        'Ceiling Lamps'
-    ],
-    homeDecor: [
-        'Plants & Planters',
-        'Aromas & Candles',
-        'Clocks',
-        'Mirrors',
-        'Wall Decor',
-        'Festive Decor',
-        'Pooja Essentials',
-        'Wall Shelves',
-        'Fountains',
-        'Showpieces & Vases',
-        'Ottoman'
-    ],
-    kitchenAndTable: [
-        'Table Runners',
-        'Dinnerware & Serveware',
-        'Cups and Mugs',
-        'Bakeware & Cookware',
-        'Kitchen Storage & Tools',
-        'Bar & Drinkware',
-        'Table Covers & Furnushings'
-    ],
-    storage: [
-        'Bins',
-        'Hangers',
-        'Organisers',
-        'Hooks & Holders',
-        'Laundary Bags'
-    ],
-    Brands: [
-        'H&M',
-        "Marks & Spencer",
-        'Home Centre',
-        'Spaces',
-        'D Decors',
-        'Story@Home',
-        'Pure Home & Living',
-        'Swayam',
-        'Raymond Home',
-        'Maspar',
-        'My Trident',
-        'Contina',
-        'Random',
-        'Ellementry',
-        'ROMEE',
-        'SEJ by Nisha Gupta'
-    ]
-};
-const beauty = {
-    makeup: [
-        'Lipstick',
-        'Lip Gloss',
-        'Lip Liner',
-        'Mascara',
-        'Eyeliner',
-        'Eye Shadow',
-        'Foundation',
-        'Blush',
-        'Kajal',
-        'Foundation',
-        'Nail Polish',
-        'Primer'
-    ],
-    skincareBathAndBody: [
-        'Body Wash',
-        'Body Lotion',
-        'Body Scrub',
-        'Body Mist',
-        'Body Oil',
-        'Hand & Foot Care',
-        'Hair Serum',
-        'Hair Oil',
-        'Hair Serum',
-        'Hair Mask',
-        'Lip Balm',
-        'Hand Cream'
-    ],
-    hairCare: [
-        'Shampoo',
-        'Conditioner',
-        'Hair Cream',
-        'Hair Oil',
-        'Hair Gel',
-        'Hair Color',
-        'Hair Serum',
-        'Hair Accessory'
-    ],
-    fragrances: [
-        'Perfumes',
-        'Deodrant',
-        'Body Mist'
-    ],
-    appliances: [
-        'Hair Straightner',
-        'Hair Dryer',
-        'Epilator'
-    ],
-    mensGrooming: [
-        'Trimmers',
-        'Bread Oil',
-        'Hair Wax'
-    ],
-    beautyGiftAndMakeupSet: [
-        'Beauty Gift',
-        'Makeup Kit'
-    ],
-    topBrands: [
-        'Lakme',
-        'maybelline',
-        'LOreal',
-        'Phlips',
-        'Bath & Body Works',
-        'THE BODY SHOP',
-        'Biotique',
-        'Mamaearth',
-        'MCaffeine',
-        'Nivea',
-        'Lotus Herbals',
-        'LOreal AYURVEDA',
-        'KAMA AYURVEDA',
-        'M.A.C',
-        'Forest Essentials'
-    ]
-}
 $(document).ready(function () {
-    $('body').on('mouseleave', '#navbarSupportedContent #navContentDiv', function (event)
+    $('body').on('mouseleave', '#navbarSupportedContent', function (event)
+    {
+        $("#dropdown").hide();
+    });
+    $('body').on('click', '#navbarSupportedContent #navContentDiv', function (event)
     {
         $("#dropdown").hide();
     });
@@ -396,17 +16,19 @@ $(document).ready(function () {
         var hoverText = $(this).text().toLowerCase();
         prepareCategoryDropdown(hoverText);
         $("#dropdown").show();
-        $("#profileDropdown").show();
     });
     $('body').on('mouseleave', '#profileUl', function (event)
     {
         $("#profileDropdown").hide();
     });
-    $('body').on('mouseover', '#profileUl span', function (event)
+    $('body').on('mouseover', '#profileUl #profileDropdownDiv', function (event)
     {
-        var hoverText = $(this).text().toLowerCase();
-        prepareCategoryDropdown(hoverText);
+        prepareProfileDropdown();
         $("#profileDropdown").show();
+    });
+    $('body').on('click', '#navbarSupportedContent, #profileUl', function (event)
+    {
+        
     });
 });
 
@@ -466,10 +88,6 @@ function prepareCategoryDropdown(hoverText)
         {
             prepareStudioDropdown();
         }
-        else if (hoverText === 'profile')
-        {
-            prepareProfileDropdown();
-        }
     }
     catch (err)
     {
@@ -496,7 +114,6 @@ function prepareMenDropdown(category)
         {
             topWearMainDiv.append('<li class="dropdown-item px-0">' + category.topWear[i] + '</li>');
         }
-
         var divider = $('<div>', {
             class: 'dropdown-divider'
         });
@@ -1102,6 +719,7 @@ function prepareProfileDropdown() {
     try
     {
         var dropdownDiv = $('#profileDropdown');
+        dropdownDiv.empty();
         var mainDiv = $('<div>', {
             class: 'col-12 mx-3 my-2',
             style: 'width: 300px'
@@ -1219,3 +837,20 @@ function prepareProfileDropdown() {
         console.log(e);
     }
 }
+
+$('body').on('click','#dropdown a', function() {
+    var clickedItemText = $(this).text();
+    switch(clickedItemText) {
+        case 'T-Shirts':
+            window.location.href = 'www.google.com';
+            break;
+        case 'Topwear':
+            window.location.href = 'www.google.com';
+            break;
+        case 'Bottomwear':
+            window.location.href = 'www.google.com';
+            break;
+        default:
+            break;
+    }
+});
